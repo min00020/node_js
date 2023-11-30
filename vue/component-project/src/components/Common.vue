3945ffa43af700b73171bbe12fa8689412e37280
민 <148922864+min00020@users.noreply.github.com>
2023-11-30 16:16:45 +0900

    231130 폴더정리, component

---
+++ vue/component-project/src/components/Common.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+export default{
+    components : [], //여러개의 자식 components있을 수 있어서 배열로 처리
+    data(){
+        return{
+            today: new Date()
+        }
+    },
+    computed: {
+        AfterYear(){
+            return this.today.setFullYear(this.today.getFullYear() + 1)
+        }
+    },
+    method:{},
+    watch:{},
+    created() {},
+    mounted() {}
+}
+</script>
+<style>
+
+</style>
