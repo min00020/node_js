1313fb2ec4292877c9bf335ae2621a0c747c5e4c
민 <148922864+min00020@users.noreply.github.com>
2023-11-30 12:34:53 +0900

    231130 Vue 시작

---
+++ vue/vue-test/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <VueComponent/>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import VueComponent from './components/VueComponent.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld,
+    VueComponent //등록하면 template에서 태그형태로 사용가능
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
